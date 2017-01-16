f5404958f097c909d204f88e7501b7e99a5dc94e
patela08 <patelakshar.patel98@gmail.com>
2017-01-16 15:42:16 -0500

    no message

--- app/app.childmessage.ts
+++ app/app.childmessage.ts
@@ -5,7 +5,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector : 'child-message',
-  template:`<h1>Hello from nested component</h1>`
+  template:`<h1>Hello from nested component Using angular 2</h1>`
 })
 
 export class ChildMessage { }
